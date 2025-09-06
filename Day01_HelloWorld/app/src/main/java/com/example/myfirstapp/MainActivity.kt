7ef84bb772edff9c296bc87ad09fcfd9bfed4880
Athus27 <athussilva27@gmail.com>
2025-09-06 14:53:40 -0300

    .

--- Day01_HelloWorld/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ Day01_HelloWorld/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyFirstAppTheme {
+            MyFirstAppTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
@@ -29,8 +29,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello eae $name!",
+        modifier = modifier
     )
 }
 
