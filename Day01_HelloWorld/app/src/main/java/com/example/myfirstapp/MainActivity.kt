4af1fc961dca4f82a87b5569c50a45bd656158c3
Athus27 <athussilva27@gmail.com>
2025-09-08 18:26:47 -0300

    .

--- Day01_HelloWorld/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ Day01_HelloWorld/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             MyFirstAppTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Athus")
                 }
             }
         }
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello eae $name!",
+        text = "Hello World, aeae $name!",
         modifier = modifier
     )
 }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
-        Greeting("Android")
+        Greeting("name")
     }
 }
